72797b1ad671ee3f650987a87d9c45b7d7b54742
Sindhu Inti <iamsindhuinti23@gmail.com>
2023-10-25 20:40:12 +0530

    Add grid system

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,34 +1,20 @@
-import { useEffect, useState } from "react";
-import apiClient from "../services/api-client";
-import { Text } from "@chakra-ui/react";
-
-interface Game {
-  id: number;
-  name: string;
-}
-
-interface FetchGamesResponse {
-  count: number;
-  results: Game[];
-}
+import { SimpleGrid, Text } from "@chakra-ui/react";
+import useGames from "../hooks/useGames";
+import GameCard from "./GameCard";
 
 const GameGrid = () => {
-  const [games, setGames] = useState<Game[]>([]);
-  const [error, setError] = useState();
+  const { games, error } = useGames();
 
-  useEffect(() => {
-    apiClient
-      .get<FetchGamesResponse>("/games")
-      .then((res) => setGames(res.data.results))
-      .catch((err) => setError(err.message));
-  }, []);
-  return (<>
-  {error && <Text>{error}</Text>}
-    <ul>
-        {games.map(game=><li key={game.id}>{game.name}</li>)}
-    </ul>
-  </>
-  )
+  return (
+    <>
+      {error && <Text>{error}</Text>}
+      <SimpleGrid columns={{sm:1,md:2,lg:3,xl:5}} spacing={10} padding="10px">
+        {games.map((game) => (
+          <GameCard key={game.id} game={game} />
+        ))}
+      </SimpleGrid>
+    </>
+  );
 };
 
 export default GameGrid;
